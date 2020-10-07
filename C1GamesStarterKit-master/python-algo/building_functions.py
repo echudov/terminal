8526a4eff2ce3eb1f95ffafdc8355cdd5deb8828
Elia <echudov@gmail.com>
2020-10-07 00:44:43 -0500

    fixed map turret wall pair

--- C1GamesStarterKit-master/python-algo/building_functions.py
+++ C1GamesStarterKit-master/python-algo/building_functions.py
@@ -84,12 +84,11 @@ def build_turret_wall_pair_game(game_state, turret_location, spent=0, above=True
     for wo in wall_offsets:
         coord = [wall_offsets[0] + turret_location[0], wall_offsets[1] + turret_location[1]]
         if cost + game_state.type_cost(game_state.WALL)[0] < game_state.SP:
-            game_state.attempt_spawn(game_state.TURRET)
-            cost += game_state.type_cost(game_state.TURRET)[0]
+            game_state.attempt_spawn(game_state.WALL)
+            cost += game_state.type_cost(game_state.WALL)[0]
 
-def build_turret_wall_pair_map(map, turret_location, spent=0, above=True, left=False, right=False, upgrade_wall=False, upgrade_turret=False):
-    cost = spent
-    if not game_state.map.in_arena_bounds(turret_location):
+def build_turret_wall_pair_map(map, turret_location, turret_id, wall_id, above=True, left=False, right=False, upgrade_wall=False, upgrade_turret=False):
+    if not map.in_arena_bounds(turret_location):
         util.debug_write("Attempted to build turret at " + turret_location + " but coordinate is out of bounds")
     wall_offsets = []
     if above:
@@ -98,13 +97,11 @@ def build_turret_wall_pair_map(map, turret_location, spent=0, above=True, left=F
         wall_offsets.append([-1, 0])
     if right:
         wall_offsets.append([1, 0])
-    if cost + game_state.cost(game_state.TURRET)[0] < game_state.SP and not game_state.contains_stationary_unit(location=turret_location):
-        game_state.attempt_spawn(game_state.TURRET, turret_location)
-        cost += game_state.type_cost(game_state.TURRET)[0]
+    if not map[turret_location[0], turret_location[1]]:
+        map.add_unit(turret_id, turret_location)
     else:
         return
     for wo in wall_offsets:
         coord = [wall_offsets[0] + turret_location[0], wall_offsets[1] + turret_location[1]]
-        if cost + game_state.type_cost(game_state.WALL)[0] < game_state.SP:
-            game_state.attempt_spawn(game_state.TURRET)
-            cost += game_state.type_cost(game_state.TURRET)[0]
+        if not map[coord[0], coord[1]]:
+            map.add_unit(wall_id)
