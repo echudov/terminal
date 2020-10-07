c4ae9b643b9aadbe322eaf93437f6be108629892
Madhav Aggarwal <madhav.taggarwal@gmail.com>
2020-10-08 01:05:53 +0200

    Refactor defensive to take advantage of built in can_spawn()

--- C1GamesStarterKit-master/python-algo/defensive_building_functions.py
+++ C1GamesStarterKit-master/python-algo/defensive_building_functions.py
@@ -28,61 +28,29 @@ class DefensiveWallStrat:
         @return: num_walls: The number of walls actually built
         """
 
-        if not game_state.map.in_arena_bounds(location=starting_location):
-            debug_write(
-                "Attempted to build wall at "
-                + starting_location
-                + " but starts out of bounds"
-            )
-            return
-
-        if game_state.number_affordable(game_state.WALL) < length:
-            debug_write(
-                "Attempted to build wall at "
-                + starting_location
-                + " but wall is too expensive"
-            )
-            return
-
         built = 0  # To return
 
         if right:
-            # Adjust location for edge (if adding length 3, want to check 3 locations, not 4th)
-            if ignore_boundaries or game_state.map.in_arena_bounds(
-                location=(starting_location[0], starting_location[1] + length - 1)
-            ):
-                built = game_state.attempt_spawn(
-                    game_state.WALL,
-                    [
-                        [starting_location[0], starting_location[1] + i]
-                        for i in range(length)
-                    ],
-                )
-            else:
-                debug_write(
-                    "Attempted to build wall at "
-                    + starting_location
-                    + " but wall goes out of bounds"
-                )
+            locations = [
+                [starting_location[0], starting_location[1] + i] for i in range(length)
+            ]
+
+            for loc in locations:
+                if not game_state.can_spawn(game_state.WALL, loc):
+                    continue
+
+                built += game_state.attempt_spawn(game_state.WALL, loc)
 
         if not right:
-            # Adjust location for edge (if adding length 3, want to check 3 locations, not "-1th")
-            if ignore_boundaries or game_state.map.in_arena_bounds(
-                location=(starting_location[0], starting_location[1] - length + 1)
-            ):
-                built = game_state.attempt_spawn(
-                    game_state.WALL,
-                    [
-                        [starting_location[0], starting_location[1] - i]
-                        for i in range(length)
-                    ],
-                )
-            else:
-                debug_write(
-                    "Attempted to build wall at "
-                    + starting_location
-                    + " but wall goes out of bounds"
-                )
+            locations = [
+                [starting_location[0], starting_location[1] - i] for i in range(length)
+            ]
+
+            for loc in locations:
+                if not game_state.can_spawn(game_state.WALL, loc):
+                    continue
+
+                built += game_state.attempt_spawn(game_state.WALL, loc)
 
         return built
 
@@ -107,49 +75,23 @@ class DefensiveWallStrat:
         @return: nothing
         """
 
-        if not game_map.in_arena_bounds(location=starting_location):
-            debug_write(
-                "Attempted to build wall at "
-                + starting_location
-                + " but wall starts out of bounds"
-            )
-            return
-
         if right:
-            # Adjust location for edge (if adding length 3, want to check 3 locations, not 4th)
-            if ignore_boundaries or game_map.in_arena_bounds(
-                location=(starting_location[0], starting_location[1] + length - 1)
-            ):
-                for loc in [
-                    [starting_location[0], starting_location[1] + i]
-                    for i in range(length)
-                ]:
-                    if not game_map[loc[0], loc[1]]:
-                        game_map.add_unit(wall_id, loc)
-            else:
-                debug_write(
-                    "Attempted to build wall at "
-                    + starting_location
-                    + " but wall goes out of bounds"
-                )
+            locations = [
+                [starting_location[0], starting_location[1] + i] for i in range(length)
+            ]
+
+            for loc in locations:
+                if not game_map[loc[0], loc[1]]:
+                    game_map.add_unit(wall_id, loc)
 
         if not right:
-            # Adjust location for edge (if adding length 3, want to check 3 locations, not "-1th")
-            if ignore_boundaries or game_map.in_arena_bounds(
-                location=(starting_location[0], starting_location[1] - length + 1)
-            ):
-                for loc in [
-                    [starting_location[0], starting_location[1] - i]
-                    for i in range(length)
-                ]:
-                    if not game_map[loc[0], loc[1]]:
-                        game_map.add_unit(wall_id, loc)
-            else:
-                debug_write(
-                    "Attempted to build wall at "
-                    + starting_location
-                    + " but wall goes out of bounds"
-                )
+            locations = [
+                [starting_location[0], starting_location[1] - i] for i in range(length)
+            ]
+
+            for loc in locations:
+                if not game_map[loc[0], loc[1]]:
+                    game_map.add_unit(wall_id, loc)
 
 
 class DefensiveTurretWallStrat:
@@ -182,14 +124,6 @@ class DefensiveTurretWallStrat:
             built (int): Number of structures built (1 wall & 1 turret) = 2
         """
 
-        if not game_state.map.in_arena_bounds(turret_location):
-            debug_write(
-                "Attempted to build turret at "
-                + turret_location
-                + " but coordinate is out of bounds"
-            )
-            return 0
-
         built = 0  # To return
 
         wall_offsets = []
@@ -200,31 +134,30 @@ class DefensiveTurretWallStrat:
         if right:
             wall_offsets.append([1, 0])
 
-        total_cost = game_state.type_cost(game_state.TURRET)[0] + (
-            game_state.type_cost(game_state.WALL)[0] * len(wall_offsets)
-        )
-        can_build_turret = not game_state.contains_stationary_unit(
-            location=turret_location
-        )
+        if not game_state.can_spawn(game_state.TURRET, turret_location):
+            return built
+        for wo in wall_offsets:
+            if not game_state.can_spawn(
+                game_state.WALL,
+                [wo[0] + turret_location[0], wo[1] + turret_location[1]],
+            ):
+                return built
 
-        if total_cost < game_state.get_resource(game_state.SP) and can_build_turret:
-            game_state.attempt_spawn(
-                game_state.TURRET, turret_location
-            )  # Build the turret
+        # Can build Turret and wall(s)
+
+        game_state.attempt_spawn(game_state.TURRET, turret_location)
+        built += 1
+
+        # Build the wall(s)
+        for wo in wall_offsets:
+            coord = [
+                wo[0] + turret_location[0],
+                wo[1] + turret_location[1],
+            ]
+            game_state.attempt_spawn(game_state.WALL, coord)
             built += 1
 
-            # Build the wall(s)
-            for wo in wall_offsets:
-                coord = [
-                    wo[0] + turret_location[0],
-                    wo[1] + turret_location[1],
-                ]
-                game_state.attempt_spawn(game_state.WALL, coord)
-                built += 1
-
-            return built
-        else:
-            return 0
+        return built
 
     def simulate_turret_wall_pair(
         self,
@@ -251,14 +184,6 @@ class DefensiveTurretWallStrat:
             upgrade_turret: Whether to upgrade the turret
         """
 
-        if not game_map.in_arena_bounds(turret_location):
-            debug_write(
-                "Attempted to build turret at "
-                + turret_location
-                + " but coordinate is out of bounds"
-            )
-            return
-
         wall_offsets = []
         if above:
             wall_offsets.append([0, 1])
@@ -270,7 +195,7 @@ class DefensiveTurretWallStrat:
         if not game_map[turret_location[0], turret_location[1]]:
             game_map.add_unit(turret_id, turret_location)
         else:
-            return
+            return  # Either add both or none
 
         for wo in wall_offsets:
             coord = [
